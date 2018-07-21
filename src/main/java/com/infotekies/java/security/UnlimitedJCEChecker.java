d2815a64d762533570576532b38ed6df65c7cfd1
Selvamohan Neethiraj <sneethir@apache.org>
2018-07-20 23:28:18 -0400

    Added Number Formatter for the JCE check

--- src/main/java/com/infotekies/java/security/UnlimitedJCEChecker.java
+++ src/main/java/com/infotekies/java/security/UnlimitedJCEChecker.java
@@ -2,6 +2,8 @@ package com.infotekies.java.security;/**
  * Created by sneethiraj on 3/14/16.
  */
 
+import java.text.NumberFormat;
+
 import javax.crypto.Cipher;
 
 public class UnlimitedJCEChecker {
@@ -12,7 +14,8 @@ public class UnlimitedJCEChecker {
             Cipher cipher = Cipher.getInstance(algorithm) ;
             int maxKeyLen = Cipher.getMaxAllowedKeyLength(algorithm);
             String msg = (maxKeyLen > 128) ? "Yes: UnlimitedJCE available." : "No: UnlimitedJCE not available." ;
-            System.out.println( msg + " {" + algorithm + "} Max KeyLength: [" + maxKeyLen + "]");
+            NumberFormat nf = NumberFormat.getInstance() ;
+            System.out.println( msg + " {" + algorithm + "} Max KeyLength: [" + nf.format(maxKeyLen) + "]");
         } catch (Exception e){
           throw new RuntimeException("Exception in finding maxLength for algorithm {" + algorithm + "}" , e) ;
         }
