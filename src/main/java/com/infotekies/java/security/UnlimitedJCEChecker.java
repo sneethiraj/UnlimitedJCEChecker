1796509d068d012ace99e2232ff09db25faa59d2
Selvamohan Neethiraj <sneethir@apache.org>
2024-03-31 18:08:02 -0400

    Formated Java Code to print the message in a nicer format.

--- src/main/java/com/infotekies/java/security/UnlimitedJCEChecker.java
+++ src/main/java/com/infotekies/java/security/UnlimitedJCEChecker.java
@@ -1,6 +1,9 @@
-package com.infotekies.java.security;/**
- * Created by sneethiraj on 3/14/16.
- */
+
+package com.infotekies.java.security;
+
+/**
+* Created by sneethiraj on 3/14/16.
+*/
 
 import java.text.NumberFormat;
 
@@ -8,18 +11,29 @@ import javax.crypto.Cipher;
 
 public class UnlimitedJCEChecker {
 
-    public static void main(String args[]) {
-        String algorithm = "AES" ;
-        try {
-            Cipher cipher = Cipher.getInstance(algorithm) ;
-            int maxKeyLen = Cipher.getMaxAllowedKeyLength(algorithm);
-            String msg = (maxKeyLen > 128) ? "Yes: UnlimitedJCE available." : "No: UnlimitedJCE not available." ;
-            NumberFormat nf = NumberFormat.getInstance() ;
-	    System.out.println("\n\n\n") ;
-            System.out.println( msg + " {" + algorithm + "} Max KeyLength: [" + nf.format(maxKeyLen) + "]");
-	    System.out.println("\n\n\n") ;
-        } catch (Exception e){
-          throw new RuntimeException("Exception in finding maxLength for algorithm {" + algorithm + "}" , e) ;
-        }
+  public static void main(String args[]) {
+    final String CIPHER_ALGO = "AES";
+    NumberFormat nf = NumberFormat.getInstance();
+    try {
+      int maxKeyLen = Cipher.getMaxAllowedKeyLength(CIPHER_ALGO);
+      boolean isJCEInstalled = (maxKeyLen > 128) ;
+      
+      StringBuffer sb = new StringBuffer() ;
+      
+      if (isJCEInstalled) {
+        sb.append("UnlimitedJCE is available.")  ;
+      }
+      else {
+        sb.append("UnlimitedJCE is NOT available.") ;
+      }
+      sb.append("\n") ;
+      
+      sb.append("{").append(CIPHER_ALGO).append("}").append(" - ") ;
+      sb.append("Max KeyLength: [").append(nf.format(maxKeyLen)).append("]").append("\n") ;
+      
+      System.out.println(sb.toString());
+    } catch (Exception e) {
+      throw new RuntimeException("Exception in finding maxLength for algorithm {" + CIPHER_ALGO + "}", e);
     }
+  }
 }
